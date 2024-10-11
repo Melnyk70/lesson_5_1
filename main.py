a9fb5702fe6ce47ef4c1ba575c92339bd1358a1a
MelnykIhor <melnyk70@gmail.com>
2024-10-11 21:35:04 +0300

    lesson_5_1

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-#https://github.com/Melnyk70/lesson_5_1.git
+#ДЗ 5.1. Ім'я змінної
+import string
+import keyword
+Lpunctuation=str(string.punctuation.replace("_"," "))
+var="tt_uu"
+var_lower=var.islower()
+i=0
+print(var,"=>", end="")
+for i in range(len(var)):
+       if var[i] in Lpunctuation:
+           print("False")
+           i=i+1
+           break
+if var in keyword.kwlist:
+   print("False ")
+elif var.count("_")>1:
+   print("False")
+elif var_lower==False:
+   if var=="_":
+       print("True")
+   else:
+       print("False")
+elif var[0] in string.digits:
+   print("False")
+else:
+   print("True")
