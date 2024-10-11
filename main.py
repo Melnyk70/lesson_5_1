21876826c34c6093c759cd7aa143734c523d76da
MelnykIhor <melnyk70@gmail.com>
2024-10-11 21:51:21 +0300

    lesson_5_1

--- main.py
+++ main.py
@@ -2,25 +2,22 @@
 import string
 import keyword
 Lpunctuation=str(string.punctuation.replace("_"," "))
-var="j7"
+var="uu uu"
 var_lower=var.islower()
 i=0
 print(var,"=>", end="")
-for i in range(len(var)):
-       if var[i] in Lpunctuation:
-           print("False")
-           i=i+1
-           break
-if var in keyword.kwlist:
-   print("False ")
+if " " in var:
+    print("False ")
+elif var in keyword.kwlist:
+    print("False ")
 elif var.count("_")>1:
-   print("False")
+    print("False")
 elif var_lower==False:
-   if var=="_":
-       print("True")
-   else:
-       print("False")
+    if var=="_":
+        print("True")
+    else:
+        print("False")
 elif var[0] in string.digits:
-   print("False")
+       print("False")
 else:
-   print("True")
+       print("True")
